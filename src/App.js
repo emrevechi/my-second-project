12a881a29fe0bd7152e234a4da858fc0208be556
Emre Vechi <emrevechi@Emres-MacBook-Pro.local>
2022-12-28 23:13:14 +0100

    updated the comment in app js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Welcome to Emre's Workshop on Github
+          This is a Demo Project which we use in our workshop
         </p>
         <a
           className="App-link"
