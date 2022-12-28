443d96fe730fdc2016465a038e36e0fe3a1431f3
Emre Vechi <emrevechi@Emres-MacBook-Pro.local>
2022-12-28 21:00:59 +0100

    updated the app js file

--- src/App.js
+++ src/App.js
@@ -7,11 +7,11 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Welcome to Emre's Workshop on Github
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://ve-chi.com"
           target="_blank"
           rel="noopener noreferrer"
         >
